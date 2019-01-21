cf1c507edf91141c64b2fe3936ab86deee5fcbf0
myeongs14 <audtjr9514@gmail.com>
2019-01-21 19:28:40 +0900

    19.01.21

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # LikeLion_Manage_Study
 
-멋쟁이 사자처럼 7기 운영진 공부 자d Python, Django
+### 멋쟁이 사자처럼 7기 운영진 공부 자료입니다.
+
+* #### Python
+
+* #### Django
+
+*  #### Flask
--- Word Count/Word Count/app/views.py
+++ Word Count/Word Count/app/views.py
@@ -9,8 +9,12 @@ def about(request):
 
 def result(request):
     text = request.GET['fulltext']
+    
+    length = list(text)
+    count = length.count(' ')
+    for x in range(0,count):
+        length.remove(' ')
     words = text.split()
-
     word_dictionary = {}
 
     for word in words:
@@ -19,11 +23,10 @@ def result(request):
         else:
             word_dictionary[word] = 1
 
-    return render(request, 'result.html', {'full': text, 'total' : len(words), 'dictionary': word_dictionary.items()})
+    return render(request, 'result.html', {'full': text, 'total' : len(words), 'dictionary': word_dictionary.items(), 'textlength': len(length),'totaltextlength': len(text) })
     
 def count(request):
     full_text = request.GET['fulltext']
-
     word_list = full_text.split()
 
-    return render(request, 'wordcount/count.html', {'fulltext': full_text, 'total': len(word_list)})
+    return render(request, 'wordcount/count.html', {'fulltext': full_text, 'total': len(word_list) })
