5d42e755ded09326a726305d2b9066b94810e51d
myeongs14 <audtjr9514@gmail.com>
2019-01-23 13:58:00 +0900

    19.01.23

--- Model Practice/practice/blog/models.py
+++ Model Practice/practice/blog/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Blog(modles.Model):
-    title =models.charFireld(max_length=200) 
-    pub_date - models.DateTimefield('date published')
-    body = models.TextField()
+class Blog(models.Model):
+    title = models.CharField(max_length=200) # 최대 200개의 문자로 되어있는 데이터
+    pub_date = models.DateTimeField('date published') # 날짜와 시간으로 되어있는 데이터
+    body = models.TextField() # 캐릭터형보다 긴 데이터
 
-    #
+    def __str__(self):
+        return self.title
+
--- Model Practice/practice/blog/views.py
+++ Model Practice/practice/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
+
+def home(request):
+    blogs = Blog.objects    # 블로그 객체를 블로그 변수로 받음
+                            # 쿼리셋 : 객체들의 목록을 전달 받음
+    return render(request, 'home.html', {'blogs' : blogs})
 
 # Create your views here.
