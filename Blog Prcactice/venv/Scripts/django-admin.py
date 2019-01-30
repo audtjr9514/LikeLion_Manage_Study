fa92d3633c42c690ed5306128b6e017d789db351
myeongs14 <audtjr9514@gmail.com>
2019-01-30 20:13:52 +0900

    19.01.30

---
+++ Blog Prcactice/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\2. 멋쟁이 사자처럼\2019년 7기 운영진\운영진 교육\blog prcactice\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
